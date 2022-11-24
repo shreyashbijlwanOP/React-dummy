208b4dfe9c92ef6c54f2b05931487220c711b626
shreyashbijlwanOP <shreyash.bijlwanmail@gmail.com>
2022-11-24 16:34:58 +0530

    Added new

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="App-header container-fluid bg-primary">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="display-3 text-light text-center p-2">
+              Welcome to my First React Page
+            </h1>
+          </div>
+        </div>
+      </header>
+  )
+}
+
+export default Header
---
+++ src/Components/Items.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Batch from "../Batch";
+const Items = () => {
+  return (
+    <ul>
+      <li>I have done my Post graduation (MCA) in 2022</li>
+      <li>Done my MCA From HNBGU garhwal</li>
+      <li>I love technology, and always try to find way to learn them.</li>
+      <li>I am from mountains. Yes, Satpuli Pauri garhwal Uttarakhand</li>
+      <li>I enrolled RFA in sept</li>
+      <Batch />
+      <li>My Main Programming Languages Are C++ and JS</li>
+      <li>I am also A Ex-Nvodian</li>
+    </ul>
+  );
+};
+
+export default Items;
